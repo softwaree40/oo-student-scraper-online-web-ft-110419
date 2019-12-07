87b3b33a62f2094332cf4ca13a6ee2400fc95171
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-07 20:16:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,18 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  students = []
-  def self.scrape_index_page(index_url)
-     page = Nokogiri::HTML(open(index_url))
-     pages = page.css("div.student-card")
-     students = []
 
-      pages.css("div.student-card").each do |student|
+  def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    # The return value of this method should be an array of hashes in which each hash represents a single student. The keys of the individual student hashes should be :name, :location and :profile_url
+    #name: doc.css("div.student-card").css("div.card-text-container").css("h4.student-name").text
+    # location: doc.css("div.student-card").css("div.card-text-container").css("p.student-location").text
+    # profile_url: doc.css("div.student-card").css("a").attribute("href").value
+
+     # profile_url: index_url.gsub("index.html", student.css("a").attribute("href").value )
+    students = []
+
+    doc.css("div.student-card").each do |student|
       students << {
         name: student.css("div.card-text-container").css("h4.student-name").text.strip,
         location: student.css("div.card-text-container").css("p.student-location").text.strip,
@@ -16,20 +21,17 @@ class Scraper
       }
     end
     students
-    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-      social_link = Nokogiri::HTML(open(profile_url))
-      pages_profile = social_link.css("div.social-icon-container a")
-      collected_link = pages_profile.collect do |link|
-      link.attribute("href")
-      
-     end
-     #binding.pry
-  end
-     student = {}
-     collected_link.each do |link|
+    doc = Nokogiri::HTML(open(profile_url))
+    social_links = doc.css("div.social-icon-container a").collect do |l|
+      l.attribute('href').text
+    end
+    student = {}
+
+    # add social links
+    social_links.each do |link|
       if link.include?("twitter")
         student[:twitter] = link
       elsif link.include?("github")
@@ -40,10 +42,11 @@ class Scraper
         student[:blog] = link
       end
     end
-     student[:profile_quote] = doc.css("div.profile-quote").text
+
+    # add profile quote & bio
+    student[:profile_quote] = doc.css("div.profile-quote").text
     student[:bio] = doc.css("div.bio-content.content-holder").css("div.description-holder p").text
 
     student
-    end
+  end
 end
-
