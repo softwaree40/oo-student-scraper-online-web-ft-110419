ae84b995e441670f14467351c340d6def46e6e4e
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-07 19:58:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,8 @@ class Scraper
   def self.scrape_profile_page(profile_url)
       social_link = Nokogiri::HTML(open(profile_url))
       pages_profile = social_link.css("div.social-icon-container")
-      linkcollect = pages_profile.collect do |link|
+      pages_profile.collect do |link|
+        link.attribute("href").text
       binding.pry
      end
   end
