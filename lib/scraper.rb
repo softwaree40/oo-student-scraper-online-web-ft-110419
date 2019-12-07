e83bc3de273b3cd2bfcfe0b1c7198ebd0a373f6a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-07 19:25:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
      page = Nokogiri::HTML(open(index_url))
      pages = page.css("div.student-card")
-      page.css("div.student-card").each |student|
+      page.css("div.student-card").each do |student|
      
      
       end
