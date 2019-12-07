deb97b80fbd64379641732b450f5ff5d4ce2f9f1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-07 19:44:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,19 +6,16 @@ class Scraper
   def self.scrape_index_page(index_url)
      page = Nokogiri::HTML(open(index_url))
      pages = page.css("div.student-card")
-     page.css("div.student-card").each do |student|
-        
+     students = []
+
+    doc.css("div.student-card").each do |student|
       students << {
-      name: student.css("div.card-text-container h4.student-name"),
-      location: student.css("div.card-text-container p.student-location"),
-      profile_url: student.css("a").attribute("href").value
-      
+        name: student.css("div.card-text-container").css("h4.student-name").text.strip,
+        location: student.css("div.card-text-container").css("p.student-location").text.strip,
+        profile_url: student.css("a").attribute("href").value
       }
-       
-     
-      end
-      students
-      
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
